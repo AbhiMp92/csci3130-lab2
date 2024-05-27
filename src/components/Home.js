afb1f95884b0cb01edbb861a8da0029d2d712863
Abhi Patel <abhi.patel@ellefsontech.com>
2024-05-26 21:22:14 -0300

    adding all code

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import React, { useEffect } from 'react';
 const Home = ({ visits, setVisits }) => {
     console.log("visits", visits);
   useEffect(() => {
-    setVisits(visits++);
+    setVisits(+visits++);
   }, [setVisits, visits]);
 
   return (
